6d8afccdc3333c1cb55324f61d42b2911cf04bac
Denis Holodtsov <denis.holodtsov@gmail.com>
2025-02-06 16:39:37 +0300

    +

--- backend/src/main/java/com/marketplace/MarketplaceApplication.java
+++ backend/src/main/java/com/marketplace/MarketplaceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Import(InfoConfiguration.class)
 //@EnableScheduling
 public class MarketplaceApplication {
 
@@ -26,19 +27,6 @@ public class MarketplaceApplication {
 /*
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
+//TIP To <b>Run</b> c
  */
+
